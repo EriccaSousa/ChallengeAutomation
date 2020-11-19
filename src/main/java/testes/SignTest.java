8c424662e542c23e532c48b043b0445cd26c2bbf
Érica Sousa <ericams175@gmail.com>
2020-11-19 18:44:34 -0300

    Finalizando.

--- src/main/java/testes/SignTest.java
+++ src/main/java/testes/SignTest.java
@@ -1,5 +1,9 @@
 package testes;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.concurrent.TimeUnit;
+
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -10,16 +14,27 @@ public class SignTest {
 	public void testRegisterSuccessfully() {
 		System.setProperty("webdriver.chrome.driver", "/home/ericas/Área de trabalho/Driver/chromedriver");
 
-		WebDriver navegador = new ChromeDriver();
+		try {
+			WebDriver navegador = new ChromeDriver();
+			navegador.manage().timeouts().implicitlyWait(6, TimeUnit.SECONDS);
 
-		navegador.get("https://www.organizze.com.br/");
+			navegador.get("https://www.organizze.com.br/");
 
-		navegador.findElement(By.linkText("Cadastre-se")).click();
-		navegador.findElement(By.name("email")).sendKeys("ericausuarioteste@gmail.com");
-		navegador.findElement(By.name("password")).sendKeys("testaut2020");
-		navegador.findElement(By.name("passwordConfirmation")).sendKeys("test123");
-		navegador.findElement(By.name("termsOfuse")).click();
-		navegador.findElement(By.tagName("button")).click();
+			navegador.findElement(By.linkText("Cadastre-se")).click();
+			navegador.findElement(By.name("email")).sendKeys("usuarioteste12346@gmail.com");
+			navegador.findElement(By.name("password")).sendKeys("test123");
+			navegador.findElement(By.name("passwordConfirmation")).sendKeys("test123");
+			navegador.findElement(By.name("termsOfuse")).click();
+			navegador.findElement(By.tagName("button")).click();
 
+			String text = navegador.findElement(By.tagName("p")).getText();
+
+			assertEquals("Parabéns! O Organizze já está preparado para você!\n"
+					+ "Enviamos um e-mail para sua caixa de entrada. Confirme seu cadastro para receber um e-mail importante da nossa equipe.",
+					text);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
